b6f54ef18dccfbcb4f6d0956567db8b022d935fd
Luis Gustavo <luisgustavodevbr@gmail.com>
2024-01-29 00:11:56 -0300

    Criando variáveis

--- main.tf
+++ main.tf
@@ -9,31 +9,43 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
-resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "br-dev-luisgustavo-my-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-    Owner       = "Luis Gustavo"
-    UpdateAt    = "2024-01-28"
-  }
+resource "aws_instance" "ec2_terraform" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+  tags = var.instance_tags
 }
 
-resource "aws_s3_bucket_ownership_controls" "my-test-bucket-controls" {
-  bucket = aws_s3_bucket.my-test-bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
 
-resource "aws_s3_bucket_acl" "my-test-bucket-acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.my-test-bucket-controls]
 
-  bucket = aws_s3_bucket.my-test-bucket.id
-  acl    = "private"
-}
+
+# resource "aws_s3_bucket" "my-test-bucket" {
+#   bucket = "br-dev-luisgustavo-my-bucket"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#     ManagedBy   = "Terraform"
+#     Owner       = "Luis Gustavo"
+#     UpdateAt    = "2024-01-28"
+#   }
+# }
+
+# resource "aws_s3_bucket_ownership_controls" "my-test-bucket-controls" {
+#   bucket = aws_s3_bucket.my-test-bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_acl" "my-test-bucket-acl" {
+#   depends_on = [aws_s3_bucket_ownership_controls.my-test-bucket-controls]
+
+#   bucket = aws_s3_bucket.my-test-bucket.id
+#   acl    = "private"
+# }
